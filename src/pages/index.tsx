5191b72416b22a3026d233279a38eb8093b543fc
Arnoud van der Heijde <avanderheijde@gmail.com>
2023-07-11 14:11:22 +0200

    Fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {false ? (hello.data ? hello.data.greeting : "Loading tRPC query...") : ""}
+              {false ? (hello.data ? hello?.data?.greeting : "Loading tRPC query...") : ""}
             </p>
             <AuthShowcase />
           </div>
